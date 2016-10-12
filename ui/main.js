ff4717d0c44a1c3062a8f4c653ef3ae183670d51
grangergirl <sbabhinaya@gmail.com>
2016-10-12 12:36:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,21 @@
-var button=document.getElementById("clicker");
-var counter=0;
+
 button.onClick=function()
 {
     counter+=1;
-    button.innerHTML=counter.toString();
-    alert("what");
+    var request=new XMLHttpRequest();
+    request.onreadystatechange= function()
+    {
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+                var counter=request.responseText;
+                var button=document.getElementById("clicker");
+                 button.innerHTML=counter.toString();
+                 alert("what");
+            }
+        }
+    };
+   
     
 };
