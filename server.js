82ab96e9a9471220f26421a3cd0f6994a47c366f
grangergirl <sbabhinaya@gmail.com>
2016-10-12 12:11:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/article-one', function (req, res) {
 
 });
 var counter=0;
-app.get('/counter', function (req, res) {
+app.get('/madder', function (req, res) {
   counter=counter+1;
   app.send(counter.toString());
 });
